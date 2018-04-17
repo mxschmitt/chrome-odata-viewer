2cf1e306926226b2624fb651f98cf81439171f7f
Max Schmitt <max@schmitt.mx>
2018-04-17 15:47:05 +0200

    Minor changes

--- devtools.js
+++ devtools.js
@@ -1,20 +1,55 @@
+var deleteKey = require('key-del')
+
+let removeQuotesIfExist = value => {
+    let match = /^'(.*)'$/.exec(value)
+    if (match) {
+        return match[1]
+    }
+    return value
+}
+
 chrome.devtools.network.onRequestFinished.addListener(data => {
     if (data.request.url.includes("$batch")) {
         let urlParser = document.createElement('a')
         urlParser.href = data.request.url
-        data.getContent((content, encoding) => {
-            let rawBody = encoding === "base64" ? atob(content) : content
-            let match = /({.*})/.exec(rawBody)
-            if (match.length == 0) {
+        data.getContent((responseContent, responseEncoding) => {
+            let rawResponseBody = responseEncoding === "base64" ? atob(responseContent) : responseContent
+            let matchPayload = /({.*})/.exec(rawResponseBody)
+            if (matchPayload.length == 0) {
                 return
             }
-            let body = JSON.parse(match[0])
-            if ('d' in body) {
-                body = body.d
+            let name,
+                kind,
+                requestData
+            let matchFunctionCall = /GET (\w+)\?(.*) HTTP/.exec(data.request.postData.text)
+            if (matchFunctionCall) {
+                kind = "Function Call"
+                name = matchFunctionCall[1]
+                requestData = matchFunctionCall[2].split("&").map(item => ({
+                    key: item.split("=")[0],
+                    value: removeQuotesIfExist(item.split("=")[1])
+                }));
             }
+            let matchOdataCall = /GET (\w+)\((.*')\)/.exec(data.request.postData.text)
+            if (matchOdataCall) {
+                kind = "oData read"
+                name = matchOdataCall[1]
+                requestData = matchOdataCall[2].split(",").map(item => ({
+                    key: item.split("=")[0],
+                    value: removeQuotesIfExist(item.split("=")[1])
+                }))
+            }
+            let responseBody = JSON.parse(matchPayload[0])
+            if ('d' in responseBody) {
+                responseBody = responseBody.d
+            }
+            responseBody = deleteKey(responseBody, "__metadata")
             let eventData = {
                 path: urlParser.pathname,
-                body: body,
+                responseBody: responseBody,
+                requestData: requestData,
+                kind: kind,
+                name: name,
                 timestamp: data.startedDateTime
             }
             console.debug("Sending message to the ui5 tab", eventData)
