dd52909fc905c73b20bbea52b31051d4a6cbeedb
Max Schmitt <max@schmitt.mx>
2018-04-20 14:32:52 +0200

    Minor changes: - Added Copy to Clipboard (fix #9) - Fixed when multiple http calls at once (fix #10)

--- devtools.js
+++ devtools.js
@@ -9,17 +9,27 @@ let removeQuotesIfExist = value => {
 }
 
 let unmarshalJSONIfPossible = data => {
+    if (data === null) {
+        return null
+    }
+    if (Array.isArray(data)) {
+        for (let i = 0; i < data.length; i++) {
+            let tmp = data[i]
+            data[i] = unmarshalJSONIfPossible(data[i])
+            console.log(`Converted  ${tmp} to ${data[i]}`)
+        }
+        return data
+    }
     switch (typeof data) {
         case "object":
-            Object.keys(data).forEach(key => {
-                data[key] = unmarshalJSONIfPossible(data[key])
-            })
-            break
-
-        case "array":
-            for (let i = 0; i < data.length; data++) {
-                data[i] = unmarshalJSONIfPossible(data[i])
+            if (data.length === 3) {
+                debugger
             }
+            Object.keys(data).forEach(key => {
+                let tmp = data[key]
+                data[key] = unmarshalJSONIfPossible(data[key])
+                console.log(`Converted  ${tmp} to ${data[key]}`)
+            })
             break
         case "number":
         case "boolean":
@@ -52,55 +62,64 @@ chrome.devtools.network.onRequestFinished.addListener(data => {
         urlParser.href = data.request.url
         data.getContent((responseContent, responseEncoding) => {
             let rawResponseBody = responseEncoding === "base64" ? atob(responseContent) : responseContent
-            let matchPayload = /({.*})/.exec(rawResponseBody)
-            if (!matchPayload) {
+            let matchResponsePayload = rawResponseBody.match(/({.+})/g)
+            if (!matchResponsePayload) {
                 return
             }
-            let name,
-                requestType,
-                requestData
+            let matchRequestPayload = data.request.postData.text.match(/batch_(?:\w+-\w+-\w+)(.*?)(?:\n|\r\n){3}-/gs)
+            if (!matchRequestPayload) {
+                return
+            }
+            if (matchRequestPayload.length !== matchResponsePayload.length) {
+                return
+            }
+            matchResponsePayload.forEach((responseBodyRaw, index) => {
+                let name,
+                    requestType,
+                    requestData
 
-            let matchFunctionCall = /GET (\w+)(?:\?|)(?:\(|)(.*)(?:\)|) HTTP/.exec(data.request.postData.text)
-            let matchODataCall = /GET (\w+)\((.*')\)/.exec(data.request.postData.text)
-            if (matchODataCall) {
-                requestType = "OData Read"
-                name = matchODataCall[1]
-                requestData = matchODataCall[2].split(",").map(item => ({
-                    key: item.split("=")[0],
-                    value: removeQuotesIfExist(item.split("=")[1])
-                }))
-            } else if (matchFunctionCall) {
-                requestType = "Function Import"
-                name = matchFunctionCall[1]
-                if (matchFunctionCall[2]) {
-                    requestData = matchFunctionCall[2].split("&").map(item => ({
+                let matchFunctionCall = /GET (\w+)(?:\?|)(?:\(|)(.*)(?:\)|) HTTP/.exec(matchRequestPayload[index])
+                let matchODataCall = /GET (\w+)\((.*')\)/.exec(matchRequestPayload[index])
+                if (matchODataCall) {
+                    requestType = "OData Read"
+                    name = matchODataCall[1]
+                    requestData = matchODataCall[2].split(",").map(item => ({
                         key: item.split("=")[0],
-                        value: decodeURIComponent(removeQuotesIfExist(item.split("=")[1]))
+                        value: removeQuotesIfExist(item.split("=")[1])
                     }))
+                } else if (matchFunctionCall) {
+                    requestType = "Function Import"
+                    name = matchFunctionCall[1]
+                    if (matchFunctionCall[2]) {
+                        requestData = matchFunctionCall[2].split("&").map(item => ({
+                            key: item.split("=")[0],
+                            value: decodeURIComponent(removeQuotesIfExist(item.split("=")[1]))
+                        }))
+                    }
+                } else {
+                    console.error(`could not determine request type: ${matchRequestPayload[index]}`)
+                    return
                 }
-            } else {
-                console.error(`could not determine request type: ${data.request.postData.text}`)
-                return
-            }
 
-            let responseBody = JSON.parse(matchPayload[0])
-            if ('d' in responseBody) {
-                responseBody = responseBody.d
-            }
-            responseBody = deleteKey(responseBody, "__metadata")
-            responseBody = deleteKey(responseBody, "__deferred")
-            responseBody = unmarshalJSONIfPossible(responseBody)
-            requestData = unmarshalJSONIfPossible(requestData)
-            let eventData = {
-                path: urlParser.pathname,
-                responseBody: responseBody,
-                requestData: requestData,
-                requestType: requestType,
-                name: name,
-                timestamp: new Date(data.startedDateTime).toTimeString().split(' ')[0]
-            }
-            console.debug("Sending message to the tab", eventData)
-            chrome.runtime.sendMessage(eventData)
+                let responseBody = JSON.parse(responseBodyRaw)
+                if ('d' in responseBody) {
+                    responseBody = responseBody.d
+                }
+                responseBody = deleteKey(responseBody, "__metadata")
+                responseBody = deleteKey(responseBody, "__deferred")
+                responseBody = unmarshalJSONIfPossible(responseBody)
+                requestData = unmarshalJSONIfPossible(requestData)
+                let eventData = {
+                    path: urlParser.pathname,
+                    responseBody: responseBody,
+                    requestData: requestData,
+                    requestType: requestType,
+                    name: name,
+                    timestamp: new Date(data.startedDateTime).toTimeString().split(' ')[0]
+                }
+                console.debug("Sending message to the tab", eventData)
+                chrome.runtime.sendMessage(eventData)
+            })
         })
     }
 })
